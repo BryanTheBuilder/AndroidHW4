be547d11d131f1f5749c92c8f020285f9c8c23c6
bRad-95 <bryantramirez@ac.c4q.nyc>
2018-01-30 14:37:40 -0500

    update

--- src/LinkedListStack_Queue.java
+++ src/LinkedListStack_Queue.java
@@ -1,53 +1,58 @@
 import java.util.*;
-public class LinkedListStack_Queue {
 
-    Scanner keyboard = new Scanner(System.in);
-    private LinkedList <String> stack_Queue = new LinkedList<>();
-        private String tempRemoveCopy;
-        private String tempAddCopy;
+class LinkedListStack_Queue {
 
-    public void push()
-    {
-        System.out.println("Enter String: ");
-        String userIN = keyboard.nextLine();
-        setAddCopy(userIN);
-        stack_Queue.add(userIN);
-    }
+  private Scanner keyboard = new Scanner(System.in);
+  private LinkedList<String> stack_Queue = new LinkedList<>();
+  private String tempRemoveCopy;
+  private String tempAddCopy;
 
-    public void setAddCopy(String userIN)
-    {
-        this.tempAddCopy = userIN;
+  void push() {
+    if (stack_Queue.size() == 10) {
+      System.out.println("List is full start making space ");
+    } else {
+      System.out.println("Enter String: ");
+      String userIN = keyboard.nextLine();
+      setAddCopy(userIN);
+      stack_Queue.add(userIN);
     }
+  }
 
-    public void copy()
-    {
-        stack_Queue.add(tempAddCopy);
-    }
+  private void setAddCopy(String userIN) {
+    this.tempAddCopy = userIN;
+  }
 
-    public void setTempRemovCopy( String userIN) {
-        this.tempRemoveCopy = userIN;
+  void copy() {
+    if (stack_Queue.size() == 10) {
+      System.out.println("List is full start making space ");
+    } else {
+      stack_Queue.add(tempAddCopy);
     }
+  }
 
-    public void remove() {
-        try {
-            System.out.println("Removing String.");
-            setTempRemovCopy(stack_Queue.getLast());
-            stack_Queue.removeLast();
-        }catch (NoSuchElementException e)
-        {
-            System.out.println("Empty list please add string first");
-        }
-    }
+  private void setTempRemovCopy(String userIN) {
+    this.tempRemoveCopy = userIN;
+  }
 
-    public void undo() {
-        if( tempRemoveCopy == null) {
-            System.out.println("Empty list please add string");
-        } else {
-            stack_Queue.add(tempRemoveCopy);
-        }
+  void remove() {
+    try {
+      System.out.println("Removing String.");
+      setTempRemovCopy(stack_Queue.getLast());
+      stack_Queue.removeLast();
+    } catch (NoSuchElementException e) {
+      System.out.println("Empty list please add string first");
     }
-    public void printList()
-    {
-        System.out.println(stack_Queue);
+  }
+
+  void undo() {
+    if (tempRemoveCopy == null) {
+      System.out.println("Empty list please add string");
+    } else {
+      stack_Queue.add(tempRemoveCopy);
     }
+  }
+
+  void printList() {
+    System.out.println(stack_Queue);
+  }
 }
